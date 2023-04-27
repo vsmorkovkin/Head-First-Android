f9f8480dd1974e7f9e7ff6ef7f4da4b6dcc15c00
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2023-04-27 14:41:42 +0300

    Chapter 12

--- GuessingGame/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
+++ GuessingGame/app/src/main/java/com/hfad/guessinggame/GameFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.hfad.guessinggame.databinding.FragmentGameBinding
@@ -26,18 +27,30 @@ class GameFragment : Fragment() {
         // set the viewModel property
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
-        updateScreen()
+        // use live data to update values on screen
+        viewModel.incorrectGuesses.observe(viewLifecycleOwner, Observer { newValue ->
+            binding.incorrectGuesses.text = "Incorrect guesses: $newValue"
+        })
 
-        binding.guessButton.setOnClickListener {
-            viewModel.makeGuess(binding.guess.text.toString().uppercase())
-            binding.guess.text = null
-            updateScreen()
+        viewModel.livesLeft.observe(viewLifecycleOwner) { newValue ->
+            binding.lives.text = "You have $newValue lives left"
+        }
 
-            if (viewModel.isWon() || viewModel.isLost()) {
+        viewModel.secretWordDisplay.observe(viewLifecycleOwner) { newValue ->
+            binding.word.text = newValue
+        }
+
+        viewModel.gameOver.observe(viewLifecycleOwner) { newValue ->
+            if (newValue) {
                 val action = GameFragmentDirections
                     .actionGameFragmentToResultFragment(viewModel.wonLostMessage())
                 view.findNavController().navigate(action)
             }
+        }
+
+        binding.guessButton.setOnClickListener {
+            viewModel.makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
         }
 
         return view
@@ -62,12 +75,6 @@ class GameFragment : Fragment() {
         super.onDestroyView()
         _binding = null
         Log.i("ViewModel", "GF onDestroyView")
-    }
-
-    fun updateScreen() {
-        binding.word.text = viewModel.secretWordDisplay
-        binding.lives.text = "You have ${viewModel.livesLeft} lives left."
-        binding.incorrectGuesses.text = "Incorrect guesses: ${viewModel.incorrectGuesses}"
     }
 
 }
--- GuessingGame/app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
+++ GuessingGame/app/src/main/java/com/hfad/guessinggame/GameViewModel.kt
@@ -1,23 +1,39 @@
 package com.hfad.guessinggame
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
-    val words = listOf("Android", "Activity", "Fragment")
-    val secretWord = words.random().uppercase()
-    val secretWordDisplay = MutableLiveData<String>()
-    var correctGuesses = ""
-    val incorrectGuesses = MutableLiveData("")
-    val livesLeft = MutableLiveData(8)
+    private val words = listOf("Android", "Activity", "Fragment")
+    private val secretWord = words.random().uppercase()
+
+    private val _secretWordDisplay = MutableLiveData<String>()
+    val secretWordDisplay: LiveData<String>
+        get() = _secretWordDisplay
+
+    private var correctGuesses = ""
+
+    private val _incorrectGuesses = MutableLiveData("")
+    val incorrectGuesses: LiveData<String>
+        get() = _incorrectGuesses
+
+    private val _livesLeft = MutableLiveData(8)
+    val livesLeft: LiveData<Int>
+        get() = _livesLeft
+
+    private val _gameOver = MutableLiveData(false)
+    val gameOver: LiveData<Boolean>
+        get() = _gameOver
+
 
     init {
-        secretWordDisplay.value = deriveSecretWordDisplay()
+        _secretWordDisplay.value = deriveSecretWordDisplay()
         Log.i("ViewModel", "GameViewModel created")
     }
 
-    fun deriveSecretWordDisplay(): String {
+    private fun deriveSecretWordDisplay(): String {
         var display = ""
         secretWord.forEach {
             display += checkLetter(it.toString())
@@ -25,7 +41,7 @@ class GameViewModel : ViewModel() {
         return display
     }
 
-    fun checkLetter(str: String) = when(correctGuesses.contains(str)) {
+    private fun checkLetter(str: String) = when(correctGuesses.contains(str)) {
         true -> str
         false -> "_"
     }
@@ -34,17 +50,18 @@ class GameViewModel : ViewModel() {
         if (guess.length == 1) {
             if (secretWord.contains(guess)) {
                 correctGuesses += guess
-                secretWordDisplay.value = deriveSecretWordDisplay()
+                _secretWordDisplay.value = deriveSecretWordDisplay()
             } else {
-                incorrectGuesses.value += "$guess "
-                livesLeft.value = livesLeft.value?.minus(1)
+                _incorrectGuesses.value += "$guess "
+                _livesLeft.value = livesLeft.value?.minus(1)
             }
         }
+        if (isWon() || isLost()) _gameOver.value = true
     }
 
-    fun isWon() = secretWord.equals(secretWordDisplay.value, true)
+    private fun isWon() = secretWord.equals(secretWordDisplay.value, true)
 
-    fun isLost() = (livesLeft.value ?: 0) <= 0
+    private fun isLost() = (livesLeft.value ?: 0) <= 0
 
     fun wonLostMessage(): String {
         var message = ""
