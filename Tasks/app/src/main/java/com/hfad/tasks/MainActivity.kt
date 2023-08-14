4111d954b650077c56ec72a2a8aadff917e7a5a4
vsmorkovkin-pc <vsmorkovkin@edu.hse.ru>
2023-08-14 23:55:35 +0300

    Chapter 17 Recycler View (Added toolbar with navigation)

--- Tasks/app/src/main/java/com/hfad/tasks/MainActivity.kt
+++ Tasks/app/src/main/java/com/hfad/tasks/MainActivity.kt
@@ -2,10 +2,29 @@ package com.hfad.tasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.hfad.tasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // use the toolbar like app's default app bar
+        setSupportActionBar(binding.toolBar)
+
+        // build a configuration linking the toolbar to the navigation graph
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
